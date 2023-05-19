521dbd113c46a3cce2d7179e804a5b1e5c9bea34
mikestratton <mike@mikestratton.net>
2023-05-19 04:53:56 -0400

    getter setter OOP student.py

--- student.py
+++ student.py
@@ -1,14 +1,30 @@
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+
+    @property  # Getter
+    def name(self):
+        return self._name
+
+    @property  # Getter
+    def house(self):
+        return self._house
+
+    @name.setter  # Setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    @house.setter  # Setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
 
 
 def main():
