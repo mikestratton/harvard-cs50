f840f1df482f1b01d555637f4dbefe8639670fd0
mikestratton <mike@mikestratton.net>
2023-05-19 06:01:50 -0400

    print type

--- type.py
+++ type.py
@@ -1,1 +1,23 @@
+def my_tuple():
+    x = 2
+    y = 3
+    return x, y
+
+
+print(type("Hello, world"))
 print(type(50))
+print(type(3.14))
+print(type(True))
+print(type([]))
+print(type(list()))
+a, b = 1, 4
+print(type((a, b)))
+tupl1 = 1, 2
+tupl2 = my_tuple()
+print(type(tupl1))
+print(type(tupl2))
+print(type(tupl2[1]))
+print(type({}))
+
+
+
